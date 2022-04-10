dc00c8b08e7194323a898669c4a549288baa2587
Luke Grove <89423806+lukegrove@users.noreply.github.com>
2022-04-10 17:23:18 -0600

    Initial commit, rough intergration of reference

--- Assets/PlayerCamera.cs
+++ Assets/PlayerCamera.cs
@@ -19,7 +19,7 @@ public class PlayerCamera : MonoBehaviour
     void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
-        Cursor.visible = false;
+        Cursor.visible = true;
     }
 
     // Update is called once per frame
